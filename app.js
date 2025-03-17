606f67666de15cd086b600f1ce0191a495050728
José Luiz <jlnalbertii@gmail.com>
2025-03-17 19:06:32 -0300

    funcao sortearAmigo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let amigos = [];
 
 function adicionarAmigo() {
-    const nome = document.getElementById("amigo").value.trim();
+    const nome = document.getElementById("amigo").value.trim().toUpperCase();
 
     if(nome === "") {
         alert("O campo não pode ser vazio!");
@@ -22,4 +22,25 @@ function adicionarNaLista(nome) {
 
     const inputNome = document.getElementById("amigo");
     inputNome.value = "";
+}
+
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert("É necessário pelo menos 2 amigos para realizar o sorteio!");
+        return;
+    }
+
+    const indiceSorteado = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceSorteado];
+
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = "";
+
+    const li = document.createElement("li");
+    li.textContent = `O amigo sorteado é: ${amigoSorteado}`;
+    resultado.appendChild(li);
+
+    amigos = [];
+    const listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
 }
